d40b1590649c6b8d83b48de4cac47e6a7cb841ef
Tres Seaver <tseaver@palladion.com>
2005-09-23 21:33:02 +0000

    Merge GenericSetup 1.0, stripping out redundant activity in CMFSetup.

---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" GenericSetup product unit tests.
+
+$Id: __init__.py,v 1.1.1.1 2005/08/08 19:38:37 tseaver Exp $
+"""
